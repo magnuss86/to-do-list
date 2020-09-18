70a9e38ca7a795760dd1ae2483af65ea07e9a1f2
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-09-17 22:06:00 -0700

    added html elements gor rows

---
+++ script.js
@@ -0,0 +1,24 @@
+var times = [
+    "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", 
+]
+
+
+// var container = document.querySelector('.container')
+// // console.log(container);
+
+
+// var div = document.createElement('div');
+// div.innerText = "10am";
+// container.appendChild(div)
+
+// function row() {
+
+// for (let i = 0; i < times.lentgh; index++) {
+//     var newRow = $("<div>10am</div>");
+//     $('.container').append(newRow);
+//     // console.log(newRow);
+// }
+// }
+
+// var $newdiv1 = $( "<div id='10am'></div>" )
+// $('container').append( $newdiv1 )

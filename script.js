8333cf18623a97819d6b7b17931c5b84d6cef69d
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-09-18 21:49:34 -0700

    adding click event

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // console.log("js")
+
+// var for storage
+
 let storage = window.localStorage
 
 
 var times = [
-    "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", 
+    "10", "11", "12", "1", "2", "3", "4", "5", "6", 
 ]
 
 
@@ -11,43 +14,12 @@ var times = [
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
-// create hour var
 
+// hour var
 var hour = moment().hours();
-// console.log(hour);
 
-//create save button function and push to local storage
+var currentTime = 14
 
-
-
-
-
-// var container = document.querySelector('.container')
-// // console.log(container);
-
-
-// var div = document.createElement('div');
-// div.innerText = "10am";
-// container.appendChild(div)
-
-// function row() {
-
-//     var t = 9;
-//     for (let i = 0; i < times.length; i++) {
-// var divEl = $("<div>")
-// $(".time-block").attr("id", `hour-${t}`);
-// var singleTime = $("<div class='col-md-1 hour'>").text(times[i]);
-// div.append(singleTime);
-
-
-// var textEl = $("<textarea class="col-md-8 past">)
-// var btnEl = $("<button class="col-md-2 saveBtn">)
-// var textEl = $("<textarea>")
-// textEl.
-
-
-// var textEl = $("<textarea>")
-// textEl.
 
 // var t = 9; // uniqie id tag corresponded to the single time block div
 for (let i = 10; i <= 18; i++) {
@@ -74,24 +46,26 @@ for (let i = 10; i <= 18; i++) {
     div.append(singleTime); 
 
     // append it to container
-    myContainer.append(div, textAreaEl, savBtn);
+    div.append(textAreaEl, savBtn)
+    myContainer.append(div);
 
+    
+
+   
 }
 
-$('.savBtn').on('click', function(){
+
+    
+$(".saveBtn").on('click', function(event){
+//   $("textarea").siblings(value)
+  console.log("test")
  //get the id off of this 
+
  //use id to get .val of input area
  //then save that value to localstorage, and then update the text
 
 
-   // $(this)
+   
 })
 
-
-
-
-// // }
-
-// var $newdiv1 = $( "<div id='10am'></div>" )
-// $('.container').append($newdiv1)
 

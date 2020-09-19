5de34ca7e44fa6e73d15447825c6d56dcf697329
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-09-19 10:46:13 -0700

    removed hardcode html and added ids for save and text elements

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 var storage = window.localStorage
 var saveText = []
 var times = [
-    "9", "11", "12", "1", "2", "3", "4", "5",]
+    "9", "10", "11", "12", "1", "2", "3", "4", "5",]
 
 // for loop that will generate my rows and columns dynamically and create the conditions for the calendar blocks to change color
 
@@ -13,8 +13,8 @@ for (let i = 0; i < times.length; i++) {
 var currentDay = $("#currentDay")     
 var div = $("<div class='row time-block'>");
 var timeDiv = $("<div class='col-md-2 hour'>");
-var textArea = $("<textarea class = 'col-md-8'>");
-var savBtn = $("<button class = 'col-md-2 saveBtn'>")
+var textArea = $("<textarea data-id = saveT class = 'col-md-8'>");
+var savBtn = $("<button data-id = saveT class = 'col-md-2 saveBtn'>")
 var myContainer = $(".container")
 // timeFix = i + 9
 // timeFix++
@@ -29,12 +29,17 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 var currentTime = moment().format('H');
 
 // Adding elements to page
+savBtn.attr("data-saveT", i)
+textArea.attr("data-saveT", i)
 timeDiv.text(times[i])
 savBtn.text("Save")
 myContainer.append(div);
 div.append(timeDiv);
 div.append(textArea);
 div.append(savBtn);
+
+//adding addtional classes/ids
+
 
 //changing colors
 if (times[i] < currentTime) {
@@ -50,24 +55,14 @@ else if (times[i] === currentTime) {
 
 }   
     
+ 
+
+// Click listener and push value to local storage
     
-
-
-    
-// $(".saveBtn").on('click', function(event){
-// //   $("textarea").siblings(value)
-//     event.preventDefault()  
-//   var savePush = $(this).siblings(".textarea").val
-// //   if ($(this)) {
-// //       localStorage.setItem("saveText", savePush)
-// //   }
-// //  //get the id off of this 
-
-// //  //use id to get .val of input area
-// //  //then save that value to localstorage, and then update the text
-
-// console.log($(this));
+$(".saveBtn").on('click', function(event){
+// console.log(event);
+event.preventDefault()
    
-// })
+})
 
 
